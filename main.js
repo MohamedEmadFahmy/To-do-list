b0eed0632fa0391f439e721c64ebb5642177fcba
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2022-12-01 00:37:19 +0200

    added add task text, changed font, enter to add

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let addButton = document.getElementById("Button");
+let addButton = document.getElementById("addTask");
 let inputField = document.getElementById("inputField");
 let notesContainer = document.getElementById("notesContainer");
 
@@ -10,6 +10,7 @@ addButton.addEventListener("click", function(){
     paragraph.innerHTML = inputField.value;
     notesContainer.appendChild(paragraph);
     inputField.value = "";
+    inputField.focus();
     paragraph.addEventListener("click", function(){
         paragraph.style.textDecoration = "line-through";
     })
@@ -17,3 +18,13 @@ addButton.addEventListener("click", function(){
         notesContainer.removeChild(paragraph);
     })
 })
+document.addEventListener('keypress', (event)=>{
+    // event.keyCode or event.which  property will have the code of the pressed key
+    let keyCode = event.keyCode ? event.keyCode : event.which;
+
+    // 13 points the enter key
+    if(keyCode === 13) {
+      // call click function of the buttonn 
+      addButton.click();
+    }
+});
