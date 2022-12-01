2369060f3a39c4304481d8830117d5fc81c18b40
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2022-12-01 02:59:54 +0200

    dd

--- main.js
+++ main.js
@@ -19,12 +19,7 @@ addButton.addEventListener("click", function(){
     })
 })
 document.addEventListener('keypress', (event)=>{
-    // event.keyCode or event.which  property will have the code of the pressed key
-    let keyCode = event.keyCode ? event.keyCode : event.which;
-
-    // 13 points the enter key
-    if(keyCode === 13) {
-      // call click function of the buttonn 
+    if(event.key === "Enter") {
       addButton.click();
     }
 });
