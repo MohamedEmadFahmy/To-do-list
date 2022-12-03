8b24079927d33be90480ca7079cd717689335dd2
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2022-12-03 03:25:37 +0200

    zebeen change

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-let addButton = document.getElementById("addTask");
+let addTask = document.getElementById("addTask");
 let inputField = document.getElementById("inputField");
 let notesContainer = document.getElementById("notesContainer");
 
 
-addButton.addEventListener("click", function(){
-    let paragraph = document.createElement('p');
-    paragraph.classList.add("currentnote");
-    paragraph.classList.add("paragraph-styling");
-    paragraph.innerHTML = inputField.value;
-    notesContainer.appendChild(paragraph);
-    inputField.value = "";
-    inputField.focus();
-    paragraph.addEventListener("click", function(){
-        paragraph.style.textDecoration = "line-through";
+addTask.addEventListener("click", function(){
+    // if(inputField.value != ""){
+        let taskBox = document.createElement('div');
+        taskBox.classList.add("taskBox");
+        let paragraph = document.createElement('p');
+        paragraph.classList.add("noteText");
+        paragraph.classList.add("paragraph-styling");
+        paragraph.innerHTML = inputField.value;
+        notesContainer.appendChild(taskBox);
+        taskBox.appendChild(paragraph);
+        let editButton  = document.createElement("button");
+        editButton.classList.add("editTask");
+        editButton.innerHTML = "Edit";
+        let deleteButton  = document.createElement("button");
+        deleteButton.classList.add("deleteTask");
+        deleteButton.innerHTML = "Delete";
+        taskBox.appendChild(editButton);
+        taskBox.appendChild(deleteButton);
+        inputField.value = "";
+        inputField.focus();
+    // }s
+
+    deleteButton.addEventListener("click", function(){
+        notesContainer.removeChild(taskBox);
+        inputField.value = "";
+        inputField.focus();
     })
-    paragraph.addEventListener("dblclick", function(){
-        notesContainer.removeChild(paragraph);
-    })
-})
+});
 document.addEventListener('keypress', (event)=>{
     if(event.key === "Enter") {
-      addButton.click();
+      addTask.click();
     }
 });
